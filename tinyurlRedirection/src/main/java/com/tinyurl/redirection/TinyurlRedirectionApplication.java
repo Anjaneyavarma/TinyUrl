68b5899d886a1fcf0af7c5dae12fef7446fe7672
Varma <anjivarma.penmatsa@gmail.com>
2025-08-13 17:31:59 +0530

    redis connection to redirection service

--- tinyurlRedirection/src/main/java/com/tinyurl/redirection/TinyurlRedirectionApplication.java
+++ tinyurlRedirection/src/main/java/com/tinyurl/redirection/TinyurlRedirectionApplication.java
@@ -2,8 +2,10 @@ package com.tinyurl.redirection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TinyurlRedirectionApplication {
 
 	public static void main(String[] args) {
--- tinyurlRedirection/src/main/java/com/tinyurl/redirection/service/impl/ResirectionServiceImpl.java
+++ tinyurlRedirection/src/main/java/com/tinyurl/redirection/service/impl/ResirectionServiceImpl.java
@@ -3,6 +3,7 @@ package com.tinyurl.redirection.service.impl;
 import com.tinyurl.redirection.entity.UrlTable;
 import com.tinyurl.redirection.repository.RedirectionRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,9 @@ public class ResirectionServiceImpl implements RedirectionService {
     private RedirectionRespository redirectionRespository;
 
     @Override
+    @Cacheable(key = "#shortUrl", value = "urlTables")
     public String fetchLongUrl(String shortUrl) {
+        System.out.println("Fetching from DB...");
         List<UrlTable> urlTables = redirectionRespository.findByShortUrl(shortUrl);
         return urlTables.get(0).getLongUrl();
     }
